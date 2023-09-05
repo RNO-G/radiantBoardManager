19a217f9dc88bb5e314f58283d13eed66af0a834
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-09-05 14:19:27 -0500

    build for all sample rates, document build.sh better

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-# extract version 
+#this builds all the different variants and sample rates automagically 
+# you can pass additonal arguments that are treated as extra flags to the C compiler (e.g. -DENABLE_INTERRUPT_HISTORY) 
 
+# extract version 
 ver_extract() {
  grep "#define VER_$1" radiantBoardManager.ino | sed "s/#define VER_$1//" | sed "s/\r//" | tr -d ' '
 }
@@ -15,17 +17,35 @@ VER_REV=`ver_extract REV`
 
 VERSTRING=v${VER_MAJOR}r${VER_MINOR}p${VER_REV} 
 
+
 mkdir -p ${VERSTRING} 
 
+# did we pass any arguments? pass them to arduino-cli as extra flags
+
+EXTRA_ARGS="" 
+
+for arg in "$@" ; 
+do 
+  EXTRA_ARGS="$EXTRA_ARGS  --build-property compiler.c.extra_flags=$arg"
+done 
+echo "$EXTRA_ARGS" 
+
 for variant in v1 v2 v3
-do
-  echo "Building $variant" 
-  arduino-cli compile -e -b osu-boards:samd:radiant_$variant || exit 1
-  BINFILE=${VERSTRING}/radiantBoardManager_${VERSTRING}.ino.radiant_${variant}.bin
-  cp build/osu-boards.samd.radiant_${variant}/radiantBoardManager.ino.bin ${BINFILE}
-  echo "Converting to uf2" 
-  UF2FILE=${VERSTRING}/radiant${variant}BoardManager_${VERSTRING}.uf2
-  uf2conv.py -o ${UF2FILE} ${BINFILE}
+  do 
+  for sample_rate in 2400 3200 ; 
+    do
+    echo "Building $variant@$sample_rate" 
+    samplerate_args="--build-property compiler.c.extra_flags=-DRADIANT_SAMPLE_RATE=$sample_rate"
+    echo cmdline: arduino-cli compile  -e -b osu-boards:samd:radiant_$variant $EXTRA_ARGS $samplerate_args . || exit 1
+    arduino-cli compile -e -b osu-boards:samd:radiant_$variant $EXTRA_ARGS $EXTRA_ARGS $samplerate_args . || exit 1
+    BINFILE=${VERSTRING}/radiantBoardManager_${VERSTRING}.ino.radiant_${variant}.${sample_rate}MHz.bin
+    cp build/osu-boards.samd.radiant_${variant}/radiantBoardManager.ino.bin ${BINFILE}
+    echo "Converting to uf2" 
+    UF2FILE=${VERSTRING}/radiant${variant}BoardManager_${VERSTRING}.${sample_rate}MHz.uf2
+    uf2conv.py -o ${UF2FILE} ${BINFILE}
+    args_file=${VERSTRING}/extra_args.txt
+    echo "$EXTRA_ARGS" > $args_file
+  done 
 done
 
 
